fd44b97b47036c5cab2fee12b02151b6e77b9219
Antonio Di Marco <me@antoniodimarco.eu>
2020-01-27 11:55:34 +0100

    Use API v2

--- cmd/dns.go
+++ cmd/dns.go
@@ -24,19 +24,47 @@ import (
 )
 
 const (
-	DNSAPI = "https://api.dynu.com/v1/dns/"
+	DNSAPI = "https://api.dynu.com/v2/dns/"
 )
 
 var f_domain string
+var f_id string
 var f_ipv4 string
 var f_all bool
+
+func httpCall(url string, httpVerb string, jsonStr string) {
+	req, _ := http.NewRequest(httpVerb, url, bytes.NewBuffer([]byte(jsonStr)))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GetToken()))
+	req.Header.Set("accept", "application/json")
+
+	cli := &http.Client{}
+	resp, err := cli.Do(req)
+	if err != nil {
+		fmt.Println("Client error:", err)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Reading body error:", err)
+	}
+
+	var prettyJSON bytes.Buffer
+	error := json.Indent(&prettyJSON, respBody, "", "\t")
+	if error != nil {
+		fmt.Println(string(respBody))
+		fmt.Println("JSON parse error:", error)
+		return
+	}
+
+	fmt.Println(string(prettyJSON.Bytes()))
+}
 
 // dnsCmd represents the dns command
 var dnsCmd = &cobra.Command{
 	Use:   "dns",
 	Short: "Work with dynu dynamic dns",
 	Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
+		cmd.Help()
 	},
 }
 
@@ -46,37 +74,28 @@ var addCmd = &cobra.Command{
 	Short: "Add a new dns record and IP",
 	Run: func(cmd *cobra.Command, args []string) {
 		if f_ipv4 == "" {
-				cmd.Help()
-				return
-		}
-
-		jsonStr := []byte(fmt.Sprintf(`{"name":"%s", "ipv4_address": "%s"}`, f_domain, f_ipv4))
-		body := bytes.NewBuffer(jsonStr)
-		infoURL := fmt.Sprint(DNSAPI, "add")
-		req, _ := http.NewRequest("POST", infoURL, body)
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GetToken()))
-		req.Header.Set("Content-Type", "application/json")
-
-		cli := &http.Client{}
-		resp, err := cli.Do(req)
-		if err != nil {
-			fmt.Println("Client error:", err)
-		}
-
-		respBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Reading body error:", err)
-		}
-
-		var prettyJSON bytes.Buffer
-		error := json.Indent(&prettyJSON, respBody, "", "\t")
-		if error != nil {
-			fmt.Println(string(respBody))
-			fmt.Println("JSON parse error:", error)
+			cmd.Help()
 			return
 		}
 
-		fmt.Println(string(prettyJSON.Bytes()))
+		body := fmt.Sprintf(`{"name":"%s", "ipv4Address": "%s"}`, f_domain, f_ipv4)
+		httpCall(DNSAPI, "POST", body)
+	},
+}
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Update an existing dns record and IP",
+	Run: func(cmd *cobra.Command, args []string) {
+		if f_domain == "" || f_ipv4 == "" || f_id == "" {
+			cmd.Help()
+			return
+		}
+
+		jsonStr := fmt.Sprintf(`{"name":"%s", "ipv4Address": "%s"}`, f_domain, f_ipv4)
+		infoURL := fmt.Sprint(DNSAPI, f_id)
+		httpCall(infoURL, "POST", jsonStr)
 	},
 }
 
@@ -85,37 +104,13 @@ var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete a dns record and its IPs",
 	Run: func(cmd *cobra.Command, args []string) {
-		if f_domain == "" {
-				cmd.Help()
-				return
-		}
-		deleteURL := fmt.Sprint(DNSAPI, "delete/", f_domain)
-		req, _ := http.NewRequest("GET", deleteURL, nil)
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GetToken()))
-		req.Header.Set("Content-Type", "application/json")
-
-		cli := &http.Client{}
-		resp, err := cli.Do(req)
-		if err != nil {
-			fmt.Println("Client error:", err)
+		if f_id == "" {
+			cmd.Help()
+			return
 		}
 
-		if resp.StatusCode == 200 {
-			fmt.Println("Succesfully deleted", f_domain)
-		} else {
-			respBody, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Println("Reading body error:", err)
-			}
-			var prettyJSON bytes.Buffer
-			error := json.Indent(&prettyJSON, respBody, "", "\t")
-			if error != nil {
-					fmt.Println("JSON parse error:", error)
-					return
-			}
-
-			fmt.Println(string(prettyJSON.Bytes()))
-		}
+		deleteURL := fmt.Sprint(DNSAPI, f_id)
+		httpCall(deleteURL, "DELETE", "")
 	},
 }
 
@@ -126,36 +121,14 @@ var infoCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var infoURL string
 		if f_domain != "" {
-			infoURL = fmt.Sprint(DNSAPI, "get/", f_domain)
+			infoURL = fmt.Sprint(DNSAPI, f_domain)
 		} else if f_all {
-			infoURL = fmt.Sprint(DNSAPI, "domains")
+			infoURL = DNSAPI
 		} else {
 			cmd.Help()
 			return
 		}
-		req, _ := http.NewRequest("GET", infoURL, nil)
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GetToken()))
-		req.Header.Set("Content-Type", "application/json")
-
-		cli := &http.Client{}
-		resp, err := cli.Do(req)
-		if err != nil {
-			fmt.Println("Client error:", err)
-		}
-
-		respBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Reading body error:", err)
-		}
-
-		var prettyJSON bytes.Buffer
-		error := json.Indent(&prettyJSON, respBody, "", "\t")
-		if error != nil {
-			fmt.Println("JSON parse error:", error)
-			return
-		}
-
-		fmt.Println(string(prettyJSON.Bytes()))
+		httpCall(infoURL, "GET", "")
 	},
 }
 
@@ -166,10 +139,16 @@ func init() {
 	addCmd.Flags().StringVarP(&f_domain, "domain", "d", "", "Dns record to add")
 	addCmd.Flags().StringVarP(&f_ipv4, "ip", "", "", "IPV4 to add as resolution for the domain provided")
 
+	dnsCmd.AddCommand(updateCmd)
+	updateCmd.Flags().StringVarP(&f_id, "id", "i", "", "Dns id to update")
+	updateCmd.Flags().StringVarP(&f_ipv4, "ip", "", "", "IPV4 to add as resolution for the id of the domain provided")
+	updateCmd.Flags().StringVarP(&f_domain, "domain", "d", "", "Dns record to update")
+
 	dnsCmd.AddCommand(deleteCmd)
-	deleteCmd.Flags().StringVarP(&f_domain, "domain", "d", "", "Domain to delete")
+	deleteCmd.Flags().StringVarP(&f_id, "id", "i", "", "Record ID to delete")
 
 	dnsCmd.AddCommand(infoCmd)
 	infoCmd.Flags().StringVarP(&f_domain, "domain", "d", "", "Domain to get DNS info for")
 	infoCmd.Flags().BoolVarP(&f_all, "all", "a", false, "Get DNS info on all records")
 }
+
--- cmd/get.go
+++ cmd/get.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
-	"fmt"
 	"encoding/json"
-	"github.com/spf13/cobra"
+	"fmt"
 	"github.com/adm-github/godynu/config"
-	"net/http"
+	"github.com/spf13/cobra"
 	"io/ioutil"
+	"net/http"
 )
 
 func init() {
@@ -14,47 +14,46 @@ func init() {
 }
 
 const (
-		URL = "https://api.dynu.com/v1/oauth2/token"
+	URL = "https://api.dynu.com/v2/oauth2/token"
 )
 
 type Token struct {
-		AccessToken string // `json:"accessToken"`
+	Access_Token string // `json:"accessToken"`
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "token",
 	Short: "get the user API token from dynu",
 	Run: func(cmd *cobra.Command, args []string) {
-		accessToken := GetToken()
-		fmt.Printf("Access token: %s\n", accessToken)
+		access_Token := GetToken()
+		fmt.Printf("Access token: %s\n", access_Token)
 	},
 }
 
 func GetToken() string {
-		v := config.InitializeConfig()
-		user := fmt.Sprint(v.Get("clientID"))
-		password := fmt.Sprint(v.Get("secret"))
+	v := config.InitializeConfig()
+	user := fmt.Sprint(v.Get("clientID"))
+	password := fmt.Sprint(v.Get("secret"))
+	req, err := http.NewRequest("GET", URL, nil)
+	req.SetBasicAuth(user, password)
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Accept-Language", "en_US")
 
-		req, err := http.NewRequest("GET", URL, nil)
-		req.SetBasicAuth(user, password)
-		req.Header.Set("Accept", "application/json")
-		req.Header.Set("Accept-Language", "en_US")
+	cli := &http.Client{}
+	resp, err := cli.Do(req)
+	if err != nil {
+		fmt.Println("Client error: ", err)
+	}
 
-		cli := &http.Client{}
-		resp, err := cli.Do(req)
-		if err != nil {
-				fmt.Println("Client error: ",err)
-		}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Reading body error: ", err)
+	}
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-				fmt.Println("Reading body error: ",err)
-		}
-
-		var token = new(Token)
-		err = json.Unmarshal(body, &token)
-		if err != nil {
-				fmt.Println("Unmarshal error", err)
-		}
-		return token.AccessToken
+	var token = new(Token)
+	err = json.Unmarshal(body, &token)
+	if err != nil {
+		fmt.Println("Unmarshal error", err)
+	}
+	return token.Access_Token
 }
